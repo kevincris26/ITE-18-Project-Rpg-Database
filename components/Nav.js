822d2dd9ca395433c5ffc399abfdfd4319046028
kevincris26 <140140367+kevincris26@users.noreply.github.com>
2023-07-21 14:06:07 +0800

    Nav.js

--- components/Nav.js
+++ components/Nav.js
@@ -1,20 +1,23 @@
-import { fetcher } from '../lib/api';
 import Link from 'next/link';
 import { useState } from 'react';
 import { setToken, unsetToken } from '../lib/auth';
 import { useUser } from '../lib/authContext';
 
+// Nav component displays the navigation bar and handles user authentication.
 const Nav = () => {
     const [data, setData] = useState({
         identifier: '',
         password: ''
     });
 
+    // useUser hook to access user data and loading state.
     const { user, loading } = useUser();
 
+    // Handles form submission to log in the user.
     const handleSubmit = async (e) => {
         e.preventDefault();
 
+        // Fetches user data from Strapi API for authentication.
         const responseData = await fetcher(
             `${process.env.NEXT_PUBLIC_STRAPI_URL}/auth/local`, 
             {
@@ -32,13 +35,17 @@ const Nav = () => {
         setToken(responseData);
     };
 
+    // Handles user logout.
     const logout = () => {
         unsetToken();
     }
 
+    // Handles form input changes and updates the state.
     const handleChange = (e) => {
         setData({ ...data, [e.target.name]: e.target.value });
     };
+
+    // Navigation bar UI with links and login/logout form.
     return (
         <nav
             className="
@@ -55,15 +62,16 @@ const Nav = () => {
         >
             <div>
                 <Link href="/" passHref>
-                        <img
-                            className="m-3"
-                            src="/rpg-logo.png"
-                            width={200}
-                            height={50}
-                            alt="RPG Logo"
-                        />
+                    <img
+                        className="m-3"
+                        src="/rpg-logo.png"
+                        width={200}
+                        height={50}
+                        alt="RPG Logo"
+                    />
                 </Link>
             </div>
+
             <div
                 className="hidden w-full md:flex md:items-center md:w-auto"
                 id="menu"
@@ -77,7 +85,7 @@ const Nav = () => {
                         md:pt-0 space-x-2"
                 >
                     <li>
-                        <Link href="/" className="md:p-2 py-2 block hover:text-purple-400">Home</Link> 
+                        <Link href="/" className="md:p-2 py-2 block hover:text-purple-400">Home</Link>
                     </li>
                     <li>
                         <Link href="/rpgs" className="md:p-2 py-2 block hover:text-purple-400">Rpgs</Link>
@@ -97,6 +105,7 @@ const Nav = () => {
                     {!loading && !user ? (
                         <>
                             <li>
+                                {/* Login form */}
                                 <form onSubmit={handleSubmit} className="from-inline">
                                     <input
                                         type="text"
@@ -125,10 +134,10 @@ const Nav = () => {
                             </li>
                         </>
                     ) : (
-                     ''
+                        ''
                     )}
                 </ul>
-            </div>  
+            </div>
         </nav>
     );
 }
